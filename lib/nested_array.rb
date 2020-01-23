d4e581b7f6c3187041c31c175cabea10bf1f461a
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-23 00:14:51 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,7 +31,7 @@ end
 
 def matrix_lookup(matrix, row, column)
   produce_storage_room = [CONVENTIONAL_PRODUCE,ORGANIC_PRODUCE]
-  produce_storage_room[1][1]
+  produce_storage_room[1][3]
 end
 
 def matrix_update(matrix, row, column, new_value)
